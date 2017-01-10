e9ff630f32d49c0633e3c260a9fe07f2e8952968
Vagrant <vagrant@alfred-12721>
2017-01-10 07:45:03 -0600

    Changes to be committed: 	new file:   tax 	modified:   tax.c

--- tax.c
+++ tax.c
@@ -5,7 +5,7 @@ float total = 0.0;
 short count = 0;
 short tax_percent = 6;
 
-float add_with_tax(float f);
+float add_with_tax(float f)
 {
 	float tax_rate = 1 + tax_percent / 100.0;
 	total = total + (f * tax_rate);
@@ -22,6 +22,6 @@ int main()
 		printf("Price of item: ");
 	}
 	printf("\nFinal total: %.2f\n", add_with_tax(val));
-	printf*"Number of items: %hi\n", count);
+	printf("Number of items: %hi\n", count);
 	return 0;
 }
