e796cf0c9c0fbe8d0476e22bec3207144b4bcd0c
HenriAW <henri.williams93@gmail.com>
2017-01-10 16:06:11 +0000

    Added int.c

---
+++ int.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main()
+{
+	printf("The value of INT_MAX is %i\n", INT_MAX);
+	printf("The value of INT_MIN is %i\n", INT_MIN);
+	printf("An int takes %z bytes\n", sizeof(int));
+	return 0;
+}
